4357ec100b1f784c1624a017099b2507a3a365de
cy322666 <cy322666@gmail.com>
2024-02-06 08:14:21 +0200

    build sms

--- application/app/Services/SmsHelper.php
+++ application/app/Services/SmsHelper.php
@@ -4,6 +4,10 @@ namespace App\Services;
 
 use App\Models\Core\Account;
 use CooperAV\SmsAero\SmsAero;
+use Zelenin\SmsRu\Api;
+use Zelenin\SmsRu\Auth\ApiIdAuth;
+use Zelenin\SmsRu\Client\Client;
+use Zelenin\SmsRu\Entity\Sms;
 
 class SmsHelper
 {
@@ -14,6 +18,7 @@ class SmsHelper
         return match ($account->subdomain) {
             'bbeducation' => new TargetSMS($access['login'], $access['pass']),
             'fashionfactoryschool' => new SmsAero($access['login'],$access['api_key']),
+            'maed' => new Api(new ApiIdAuth($access['api_key']), new Client()),
         };
     }
 
@@ -27,7 +32,11 @@ class SmsHelper
             'fashionfactoryschool' => [
                 'login'   => env('FF_LOGIN'),
                 'api_key' => env('FF_API_KEY'),
-            ]
+            ],
+            'maed' => [
+                'login'   => env('MAED_LOGIN'),
+                'api_key' => env('MAED_APIKEY'),
+            ],
         ];
 
         return $tokens[$subdomain];
@@ -39,7 +48,8 @@ class SmsHelper
 
         return match ($subdomain) {
             'bbeducation' => 'Ознакомиться с договором на обучение можно по ссылке '.$lead->cf('Договор. Ссылка')->getValue().'. Код подтверждения: '.$code.'. Для подписания договора введите его тут '.$lead->cf('Договор. Анкета код')->getValue().'. Cайт https://bangbangeducation.ru',
-            'fashionfactoryschool' => 'Ознакомиться с договором на обучение можно по ссылке '.$lead->cf('Договор. Ссылка')->getValue().'. Код подтверждения: '.$code.'. Для подписания договора введите его тут '.$lead->cf('Договор. Анкета код')->getValue().'. '//TODO Cайт https://bangbangeducation.ru',
+            'fashionfactoryschool' => 'Ознакомиться с договором на обучение можно по ссылке '.$lead->cf('Договор. Ссылка')->getValue().'. Код подтверждения: '.$code.'. Для подписания договора введите его тут '.$lead->cf('Договор. Анкета код')->getValue().'. ',//TODO Cайт https://bangbangeducation.ru',
+            'maed' => 'Ознакомиться с договором на обучение можно по ссылке '.$lead->cf('Договор. Ссылка')->getValue().'. Код подтверждения: '.$code.'. Для подписания договора введите его тут '.$lead->cf('Договор. Анкета код')->getValue().'. ',//TODO Cайт
         };
     }
 
@@ -68,6 +78,16 @@ class SmsHelper
 //            $idSms  = $values[1]['attributes']['ID_SMS'];
 //            $status = $values[1]['attributes']['STATUS'];
         }
+          if ($subdomain == 'maed') {
+
+            $response = new Sms($phone, $sms);
+
+            $client->smsStatus($response->ids[0]);
+
+            return [
+                'status' => $response['success'] //TODO
+            ];
+        }
     }
 
     public static function matchStatus($subdomain): int
@@ -75,6 +95,7 @@ class SmsHelper
         return match ($subdomain) {
             'bbeducation' => 59740474,
             'fashionfactoryschool' => 63581333,
+            'maed' => '',//TODO
         };
     }
 }
